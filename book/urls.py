dfbe4dea35c223fa90deca9d8af29da984b6a18d
josspheenboles <eng.jospheenboles@gmail.com>
2024-04-23 11:18:29 +0200

    List Books

--- book/urls.py
+++ book/urls.py
@@ -3,7 +3,7 @@ from .views import *
 urlpatterns=[
     path('List/',book_list,name='book_list'),
     path('Add/',book_add,name='book_add'),
-    path('Update/<str:name>/',book_update,name='book_update'),
+    path('Update/<str:title>/',book_update,name='book_update'),
     path('Delete/<int:id>/',book_delete,name='book_delete'),
     path('Details/<int:id>/',book_details,name='book_details'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def book_list(request):
-    return  render(request,'book/list.html',{})
+    context={}
+    context['books']=Book.objects.all()
+    return  render(request,'book/list.html',context)
     # return HttpResponse('<h1>List</h1')
 def book_add(request):
     return HttpResponse('<h1>Add</h1')
-def book_update(request,name):
+def book_update(request,title):
     return HttpResponse('<h1>update</h1')
 def book_delete(request,id):
     return HttpResponse('<h1>delete</h1')
