3bb6bd00ac3f19a8c9fbdcc77273fe1abfe93b96
josspheenboles <eng.jospheenboles@gmail.com>
2024-04-23 09:47:23 +0200

    views

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 urlpatterns=[
-    path('List',book_list,name='book_list'),
-    path('Add',book_add,name='book_add'),
-    path('Update/<str:name>',book_update,name='book_update'),
-    path('Delete/<int:id>',book_delete,name='book_delete'),
-    path('Details/<int:id>',book_details,name='book_details'),
+    path('List/',book_list,name='book_list'),
+    path('Add/',book_add,name='book_add'),
+    path('Update/<str:name>/',book_update,name='book_update'),
+    path('Delete/<int:id>/',book_delete,name='book_delete'),
+    path('Details/<int:id>/',book_details,name='book_details'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def book_list(request):
+    return HttpResponse('<h1>List</h1')
+def book_add(request):
+    return HttpResponse('<h1>Add</h1')
+def book_Update(request,name):
+    return HttpResponse('<h1>update</h1')
+def book_delete(request,id):
+    return HttpResponse('<h1>delete</h1')
+def book_details(request,id):
+    return HttpResponse('<h1>details</h1')
