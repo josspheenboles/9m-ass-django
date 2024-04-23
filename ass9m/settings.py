e7f9a1b22c99e12d95e5ed129196f11aeca8de11
josspheenboles <eng.jospheenboles@gmail.com>
2024-04-23 13:10:39 +0200

    upload media

--- ass9m/settings.py
+++ ass9m/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'ass9m.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME':'ass9m.sqlite',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'ass9m',
-        'USER':'root',
-        'PASSWORD':'123',
-        'HOST':'localhost',
-        'PORT':3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':'ass9m.sqlite',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME':'ass9m',
+        # 'USER':'root',
+        # 'PASSWORD':'123',
+        # 'HOST':'localhost',
+        # 'PORT':3306
     }
 }
 
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL='media/'
+MEDIA_ROOT=['/media']
