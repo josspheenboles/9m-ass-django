4dcea192c08fce20a9638549a73eb9f3e135b12f
josspheenboles <eng.jospheenboles@gmail.com>
2024-04-23 10:53:46 +0200

    register model to a

--- ass9m/settings.py
+++ ass9m/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'ass9m.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'ass9m',
+        'USER':'root',
+        'PASSWORD':'123',
+        'HOST':'localhost',
+        'PORT':3306
     }
 }
 
