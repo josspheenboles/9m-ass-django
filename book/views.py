240674e93149e5b247a9b0f8b6f1a21688f74328
josspheenboles <eng.jospheenboles@gmail.com>
2024-04-23 10:47:08 +0200

    templates inheritance

--- book/views.py
+++ book/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def book_list(request):
-    return HttpResponse('<h1>List</h1')
+    return  render(request,'book/list.html',{})
+    # return HttpResponse('<h1>List</h1')
 def book_add(request):
     return HttpResponse('<h1>Add</h1')
 def book_update(request,name):
